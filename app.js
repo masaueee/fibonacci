b338f9562568fc2ce8a11c7b0e958b206c288b0a
masaueee <masa_ueee@hotmail.co.jp>
2020-04-25 22:36:40 +0900

    improvement algorithmn

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 'use strict';
 
+const fibMap = new Map();
+fibMap.set(0,0);
+fibMap.set(1,1);
+
 function fib(n) {
-    if (n === 0) {
-      return 0;
-    }else if (n === 1) {
-    return 1;
+    if (fibMap.has(n)) {
+        return fibMap.get(n);
     }
-    return fib(n - 1) + fib(n - 2);
+    fibMap.set(n,fibMap.get(n-1) + fibMap.get(n-2))
+    return fibMap.get(n);
 }
 
 const length = 40;
-  for (let i = 0; i <= length; i++) {
-      console.log(fib(i));
-  }
+for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+}
+
+
+
